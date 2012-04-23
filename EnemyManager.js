hoorayimhelping/Galaga5

commit f0568864749132478d5f12f814f906c0308e0e29

Made the circling galaxians Hanks when even and Deans when odds

EnemyManager.js
@@ -20,9 +20,10 @@ EnemyManager.prototype.initialize = function(enemyType, enemyCount) {
     }
   }
 
-  if (enemyType.toLowerCase() === 'dean_circle') {
+  if (enemyType.toLowerCase() === 'circle_man') {
     for (var i = 0; i < enemyCount; i++) {
-      this.enemies.push(new Dean().initialize({ y: 130, x: (i + 2) * 55 }));
+      var man = i % 2 ? new Dean() : new Hank();
+      this.enemies.push(new man.initialize({ y: 130, x: (i + 2) * 55 }));
     }
   }
 
@@ -40,8 +41,8 @@ EnemyManager.prototype.circle = function(speed, centerPoint, radius) {
   var radians = (this.angle) * (Math.PI/180);
 
   for (var i = 0, l = this.enemies.length; i < l; i++) {
-    this.enemies[i].frame.y = centerPoint + Math.sin(radians) * radius;
-    this.enemies[i].frame.x = centerPoint + Math.cos(radians) * radius;
+    this.enemies[i].frame.y = centerPoint + Math.sin(radians * i) * radius;
+    this.enemies[i].frame.x = centerPoint + Math.cos(radians * (i%5)) * radius;
 
   }
   this.angle += 2.3 * speed;

GameEngine.js
@@ -29,7 +29,7 @@ GameEngine.prototype.initialize = function(canvas) {
 
   this.hankManager = new EnemyManager().initialize('hank', 4);
   this.deanManager = new EnemyManager().initialize('dean', 4);
-  this.deanCircleManager = new EnemyManager().initialize('dean_circle', 1);
+  this.deanCircleManager = new EnemyManager().initialize('circle_man', 6);
 
   // this puts the player's ship at the bottom of the screen and offsets it by the ship's height and a few extra pixels
   this.player.frame.y = this.canvas.height - this.player.frame.height * 1.1;

commit 698916fa8a89862cea265f3f0408cd08589cdc73

Fixed a bug introduced in f0568864749132478d5f12f814f906c0308e0e29 by removing new on variable already created with new. Changed variable name enemyLength to enemyCount to better reflect intent

EnemyManager.js
@@ -22,8 +22,9 @@ EnemyManager.prototype.initialize = function(enemyType, enemyCount) {
 
   if (enemyType.toLowerCase() === 'circle_man') {
     for (var i = 0; i < enemyCount; i++) {
+      console.log(enemyCount, i)
       var man = i % 2 ? new Dean() : new Hank();
-      this.enemies.push(new man.initialize({ y: 130, x: (i + 2) * 55 }));
+      this.enemies.push(man.initialize({ y: 130, x: (i + 2) * 55 }));
     }
   }
 

GameEngine.js
@@ -149,12 +149,12 @@ GameEngine.prototype.getAllParticles = function() {
  */
 GameEngine.prototype.detectCollisions = function() {
   var enemies = this.getAllEnemies(),
-    enemyLength = enemies.length,
+    enemyCount = enemies.length,
     playerBulletCount = this.playerBullets.length;
 
   for (var i = 0; i < playerBulletCount; i++) {
 	  if (this.playerBullets[i].active) {
-      for (var j = 0; j < enemyLength; j++) {
+      for (var j = 0; j < enemyCount; j++) {
         if (enemies[j].alive) {
           if (this.colliding(enemies[j], this.playerBullets[i])) {
             enemies[j].die();